e72ffe3bf32ba4d47fc4dd1456d8919d29e47eee
Daniela Alejandra Oviedo Torres <60947400+DanielaOviedo@users.noreply.github.com>
2020-09-17 00:28:24 -0500

    subiendo

---
+++ Practica1Unidad2/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Practica1Unidad2.Models;
+
+namespace Practica1Unidad2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(Datos n)
+        {
+            return View(n);
+        }
+        public IActionResult Capturar()
+        {
+            return View();
+        }
+    }
+}
---
+++ Practica1Unidad2/Models/Datos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practica1Unidad2.Models
+{
+    public class Datos
+    {
+        public int n1 { get; set; }
+        public int n2 { get; set; }
+        public int Resultado()
+        {
+            return n1 + n2;
+        }
+    }
+}
